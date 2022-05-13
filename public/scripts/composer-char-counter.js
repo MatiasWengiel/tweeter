b68d977bece9fe0e5e2882be3fbe48eb44e0f804
Matias Wengiel <matiaswengiel@gmail.com>
2022-05-13 15:10:46 -0500

    Add script using JQuery function so that the counter reflects the number of characters left for the user. Change font of counter to Bungee

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
   console.log('Server ready to dynamically generate counter');
 
   $("#tweet-text").on('input', function() {
-    console.log(140 - $(this).val().length);
+    let textLength = $(this).val().length;
+    let counter = $(this).siblings('div').children('.counter');
+
+    counter.val(function () {
+      const output = 140 - textLength;
+
+      if (output < 0) {
+        counter.css('color', 'red')
+      } else { //Needed to turn color back to black if user deletes extra characters
+        counter.css('color', 'black')
+      }
+      return output;
+    }) 
   })
 })
