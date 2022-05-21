2cddd7df5069ba3eebbb6fd41ac014568505145a
Matias Wengiel <matiaswengiel@gmail.com>
2022-05-21 18:08:47 -0500

    Move helper functions for load tweets to separate file. Improve commenting for clarity. Remove scroll function that was no longer necessary.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,68 +28,14 @@ $(document).ready(function() {
     $('#tweet-text').focus();
   })
 
-  $(window).off('scroll', function() {
-    $('.scroll-to-top').hide();
-  })
-
-  const escapeUserInputText = function(text) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(text));
-    return div.innerHTML;
-  };
-
-  const createTweetElement = function(tweet) {
-    const userName = tweet.user.name;
-    const avatar = tweet.user.avatars;
-    const handle = tweet.user.handle;
-    const tweetText = tweet.content.text;
-    const tweetTime = tweet.created_at;
-    const timeAgo = timeago.format(tweetTime);
-
-    const $tweetArticle =  `
-      <article class="tweet">
-        <header>
-          <section class="img-username">
-            <img src="${escapeUserInputText(avatar)}">
-            <p>${escapeUserInputText(userName)}</p>
-          </section>
-          <p>${escapeUserInputText(handle)}</p>
-        </header>
-
-        <p class="tweet-content">${escapeUserInputText(tweetText)}</p>
-
-        <footer>
-          <ins datetime="...">${timeAgo}</ins>
-          <menu>
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </menu>
-        </footer>
-      </article>
-    `;
-    return $tweetArticle;
-  };
-
-  let numberOfTweetsAlreadyRendered = 0;
-
-  const renderTweets = function(tweets) {
-    for (let tweet = numberOfTweetsAlreadyRendered; tweet < tweets.length; tweet++) {
-      const newTweet = createTweetElement(tweets[tweet]);
-      $('.new-tweet').after(newTweet);
-      numberOfTweetsAlreadyRendered++;
-    }
-
-  };
-
   const loadTweets = function() {
     $.get('/tweets/')
       .then(function(tweetsData) {
-        renderTweets(tweetsData);
+        renderTweets(tweetsData); //From helper-functions
       });
   };
 
-  loadTweets();
+  loadTweets(); //Initial loading of tweets
  
 
   $('form').submit(function(event) {
@@ -109,6 +55,7 @@ $(document).ready(function() {
       $('.too-many-chars').slideDown('slow');
       return;
     }
+    
     //Post tweet after validation
     $.post('/tweets/', data)
 
---
+++ public/scripts/helper-functions.js
@@ -0,0 +1,50 @@
+
+  const escapeUserInputText = function(text) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(text));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = function(tweet) {
+    const userName = tweet.user.name;
+    const avatar = tweet.user.avatars;
+    const handle = tweet.user.handle;
+    const tweetText = tweet.content.text;
+    const tweetTime = tweet.created_at;
+    const timeAgo = timeago.format(tweetTime);
+
+    const $tweetArticle =  `
+      <article class="tweet">
+        <header>
+          <section class="img-username">
+            <img src="${escapeUserInputText(avatar)}">
+            <p>${escapeUserInputText(userName)}</p>
+          </section>
+          <p>${escapeUserInputText(handle)}</p>
+        </header>
+
+        <p class="tweet-content">${escapeUserInputText(tweetText)}</p>
+
+        <footer>
+          <ins datetime="...">${timeAgo}</ins>
+          <menu>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </menu>
+        </footer>
+      </article>
+    `;
+    return $tweetArticle;
+  };
+
+  let numberOfTweetsAlreadyRendered = 0;
+
+  const renderTweets = function(tweets) {
+    for (let tweet = numberOfTweetsAlreadyRendered; tweet < tweets.length; tweet++) {
+      const newTweet = createTweetElement(tweets[tweet]);
+      $('.new-tweet').after(newTweet);
+      numberOfTweetsAlreadyRendered++;
+    }
+
+  };
